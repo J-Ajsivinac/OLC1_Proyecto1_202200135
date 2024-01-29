fd4b9a0dacca3d682f8ff04d065a41694187d94f
J-Ajsivinac <3114791110409@ingenieria.usac.edu.gt>
2024-01-29 15:22:53 -0600

    se inicia el proyecto 1

---
+++ src/proyecto/pkg1/Proyecto1.java
@@ -0,0 +1,10 @@
+package proyecto.pkg1;
+
+
+public class Proyecto1 {
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.println("Proyecto 1");
+    }
+    
+}
