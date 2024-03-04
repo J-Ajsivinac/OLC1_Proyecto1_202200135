547f5e93379b525a3aa4ca2792f0e5e2d4a2ac62
J-Ajsivinac <3114791110409@ingenieria.usac.edu.gt>
2024-03-03 21:31:30 -0600

    Añadir tipo de variable a la declaración de variables

--- src/Tools/VariableDeclaration.java
+++ src/Tools/VariableDeclaration.java
@@ -4,10 +4,25 @@ package Tools;
 public class VariableDeclaration {
     private Object id;
     private Object value;
+    private TypeVariable typeMain;
+
+    public TypeVariable getTypeMain() {
+        return typeMain;
+    }
+
+    public void setTypeMain(TypeVariable typeMain) {
+        this.typeMain = typeMain;
+    }
     
     public VariableDeclaration(Object id, Object value) {
         this.id = id;
         this.value = value;
+    }
+    
+    public VariableDeclaration(Object id, Object value, TypeVariable t) {
+        this.id = id;
+        this.value = value;
+        this.typeMain = t;
     }
 
     public Object getId() {
